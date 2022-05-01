11f164083902e01d65fc86ae61ae503e62e86552
Ntsemi Bophilo <1932442@students.wits.ac.za>
2022-05-01 13:43:23 +0200

    routing

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const path = require('path') // used later in the exercise
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-res.send('Hello World')
-})
+const mainRouter = express.Router()
 
-app.listen(3000)
-console.log('Express server running on port 3000')
+ mainRouter.get('/', function (req, res) {
+ res.send('Hello World')
+ })
 
-app.get('/about', function(req, res){
-    res.sendFile(path.join(__dirname, 'views', 'about.html'));
-    });
+ mainRouter.get('/about', function (req, res) {
+ res.sendFile(path.join(__dirname, 'views', 'about.html'))
+ })
+
+ app.use(mainRouter)
+
+ app.listen(3000)
+ console.log('Express server running on port 3000')
