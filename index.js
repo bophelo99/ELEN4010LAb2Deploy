8a87ce4f446003637341e946b108aff5a1056507
Ntsemi Bophilo <1932442@students.wits.ac.za>
2022-05-01 13:24:19 +0200

    port server

---
+++ index.js
@@ -0,0 +1,10 @@
+const path = require('path') // used later in the exercise
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+res.send('Hello World')
+})
+
+app.listen(3000)
+console.log('Express server running on port 3000')
